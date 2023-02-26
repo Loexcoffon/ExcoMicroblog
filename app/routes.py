d3ef36c6f6337dce42072fe1c4410065990bf7b2
Loexcoffon <loexcoffon@packer.edu>
2023-02-26 18:37:50 -0500

    chapter 6

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,9 @@ from flask import request
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
+from datetime import datetime
+from app.forms import EditProfileForm
+
 
 @app.route('/')
 
@@ -64,11 +67,32 @@ def songs():
     ]
     return render_template('songs.html', title='Songs', songs=songs)
 
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
 @app.route('/deck')
 def deck():
     suits = ["S", "C", "H", "D"]
     nums = ['a','2','3','4','5','6','7','8','9','t','j','q','k']
     return render_template('deck.html', title='Full Deck of Cards', suits=suits, nums=nums)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -83,3 +107,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
